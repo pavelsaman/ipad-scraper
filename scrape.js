5bcbdc6a0131730b24772245c1d1d296e6bf94c3
Pavel Saman <pavel.saman@inveo.cz>
2020-11-16 13:42:02 +0100

    Refactor main file, config files

--- scrape.js
+++ scrape.js
@@ -2,66 +2,11 @@ const puppeteer = require('puppeteer');
 const sqlite3 = require('sqlite3');
 const sqlite = require('sqlite');
 
+const config = require('./config.json');
+const pagesToScrape = require('./pagesToScrape.json');
+
 const dbFile = './prices.db';
 const date = new Date();
-const browserConfig = {
-    headless: {
-        headless: true,
-        args: [                
-            '--window-size=1920,1040'
-        ],
-        defaultViewport: null
-    },
-    gui: {
-        headless: false,
-        args: [                
-            '--start-maximized'     
-        ],
-        defaultViewport: null            
-    },
-    debug: {
-        headless: false,
-        args: [                
-            '--start-maximized'
-        ],
-        defaultViewport: null,
-        slowMo: 50,
-        devtools: true          
-    }
-};
-
-const pagesToScrape = [
-    {
-        company: 'Alza',
-        name: 'iPad 2020',
-        url: 'https://www.alza.cz/ipad-2020/18881698.htm',
-        priceEl: 'div.priceInner > span.c2'
-    },
-    {
-        company: 'Alza',
-        name: 'iPad Pro 12.9"',
-        url: 'https://www.alza.cz/ipad-pro-12-9/18858953.htm',
-        priceEl: 'div.priceInner > span.c2'
-    },
-    {
-        company: 'Alza',
-        name: 'iPad Pro 11"',
-        url: 'https://www.alza.cz/ipad-pro-11/18867629.htm',
-        priceEl: 'div.priceInner > span.c2'
-    },
-    {
-        company: 'Datart',
-        name: 'iPad Pro',
-        url: 'https://www.datart.cz/ipad-pro.html?perPage=48',
-        priceEl: '#products-list >* span.actual > span.tooltip.tooltipstered'
-    },
-    {
-        company: 'CZC',
-        name: 'iPad Pro 12.9" 2020',
-        url: 'https://www.czc.cz/ipad-pro-12-9-2020-s-ipados/produkty',
-        priceEl: '.price-vatin'
-    }
-];
 
 const getRidOfSpecialChars = (arr) => {
     let newArr = arr.map(p => p.replace(/ /g, ''));
@@ -79,7 +24,6 @@ const savePrices = async (priceObj) => {
         driver: sqlite3.Database
     });
     
-    const isoDate = date.toISOString();
     for (let p of priceObj.prices) {
         await db.run(
             'INSERT INTO Prices (name, company, url, price, datetime) VALUES (?, ?, ?, ?, ?)',
@@ -87,7 +31,7 @@ const savePrices = async (priceObj) => {
             priceObj.metaInfo.company,
             priceObj.metaInfo.url,
             p,
-            isoDate
+            date.toISOString()
         );
     }
 
@@ -96,7 +40,7 @@ const savePrices = async (priceObj) => {
 
 (async () => {
 
-    const browser = await puppeteer.launch(browserConfig.headless);
+    const browser = await puppeteer.launch(config.browserConfig.headless);
     const context = await browser.createIncognitoBrowserContext();
     const page = await context.newPage();
 
